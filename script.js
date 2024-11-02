a7ef4e07b68e9b6f1e4d16b2bde166c20e894ba4
lu1s0t3 <delgadoconde55@gmail.com>
2024-11-02 13:15:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('La página ha cargado completamente.');
-});
+// Función para redirigir después de un tiempo
+function redirectAfterDelay(redirectPage, delay) {
+    setTimeout(function() {
+        if (redirectPage) {
+            window.location.href = redirectPage;
+            localStorage.removeItem('redirectPage');
+        }
+    }, delay);
+}
+
+// Al cargar la página, verificar si hay una redirección pendiente
+window.onload = function() {
+    const redirectPage = localStorage.getItem('redirectPage');
+    if (redirectPage) {
+        redirectAfterDelay(redirectPage, 6000);
+    }
+};
