1b3ea9bc6ad509114bc0c8c589baeea931a0b206
lu1s0t3 <delgadoconde55@gmail.com>
2024-11-03 00:11:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-// Función para redirigir después de un tiempo
-function redirectAfterDelay(redirectPage, delay) {
-    setTimeout(function() {
-        if (redirectPage) {
-            window.location.href = redirectPage;
-            localStorage.removeItem('redirectPage');
-        }
-    }, delay);
-}
+// script.js
 
-// Al cargar la página, verificar si hay una redirección pendiente
+// Esta función se ejecuta al cargar la página index.html
 window.onload = function() {
     const redirectPage = localStorage.getItem('redirectPage');
     if (redirectPage) {
-        redirectAfterDelay(redirectPage, 6000);
+        setTimeout(() => {
+            window.location.href = redirectPage;
+            localStorage.removeItem('redirectPage'); // Limpiar el valor después de usarlo
+        }, 6000);
     }
 };
+
+// Redirige desde las páginas de política y eliminación de datos
+if (document.title.includes("Política de Privacidad")) {
+    localStorage.setItem('redirectPage', 'politica_privacidad.html');
+    window.location.href = "index.html";
+} else if (document.title.includes("Instrucciones para la Eliminación de Datos")) {
+    localStorage.setItem('redirectPage', 'instrucciones_eliminacion_datos.html');
+    window.location.href = "index.html";
+}
